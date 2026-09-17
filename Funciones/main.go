01bc157f92c2358bdcfb8c0a70dac1fddeb53af0
paula795 <pasimbanape@uide.edu.ec>
2026-09-17 11:27:22 -0500

    Funciones2.3

--- Funciones/main.go
+++ Funciones/main.go
@@ -2,39 +2,70 @@ package main
 
 import "fmt"
 
+/*
+func <nombre>(param1, param2,... param n)<valores de retorno>{
+	-----------------------
+	-----------------------
+	-----------------------
+	//return en el caso de que nuestra función retorne valores
+	})
+
+*/
+
 func saludar() {
-	fmt.Println("Hola esta es mi primera funcion")
+	fmt.Println("Hola, esta es mi primera función")
 }
 
-// Funcion con un parametro
-func nombre(nombre string) {
-	fmt.Println("Bienvenida/a", nombre)
-}
-func main() {
-	var usr string
-	fmt.Println("Ingresa tu nombre:")
-	fmt.Scan(&usr)
-	fmt.Println("Hola bienvenido/a:", usr)
-	fmt.Println("El resultado de la suma es:", suma(4, 5))
+func bienvenida(nombre string) {
+	fmt.Println("Bienvenid@", nombre)
 }
 
-// solicite dos numeros y resulva su suma
-func suma(a int, b int) int {
-	return a + b
-}
-
-// funcion que se llame suma resta solicitar dos numeros y retornar dos valores, resultado de la suma
-// y resta
-func sumaResta(num1, num2 int) (int, int) {
+func sumar_restar(num1 int, num2 int) (int, int) {
+	var result_sum int
+	result_sum = num1 + num2
 	if num2 > num1 {
-		ResSuma := num1 + num2
-		ResResta := num1 - num2
-		return ResSuma, ResResta
+		var result_rest int
+		result_rest = num2 - num1
+		return result_sum, result_rest
 	} else {
-		ResSuma := num1 + num2
-		ResResta := 0
-		return ResSuma, ResResta
+		fmt.Println("EL resultado es 0")
+		return result_sum, 0
 	}
 
 }
-jnjjjjnjnj
+
+/* Variadic function ========== Funciones Variadicas*/
+
+func mostrarnum(numeros ...int) {
+	fmt.Println("Los numeros ingresados son: ", numeros)
+}
+
+func sumatoria(numeros ...int) int {
+	total := 0
+	for _, numero := range numeros {
+		total += numero
+	}
+	return total
+}
+
+func main() {
+	var usr string
+	fmt.Println("Ingresa tu nombre: ")
+	fmt.Scan(&usr)
+	saludar()
+	bienvenida(usr)
+
+	//fmt.Println("El resultado de la suma es: ", sumar(5, 10))
+
+	var num1, num2 int
+	fmt.Println("Ingresa el primer número: ")
+	fmt.Scan(&num1)
+	fmt.Println("Ingresa el segundo número: ")
+	fmt.Scan(&num2)
+	resultadoSuma, resultadoResta := sumar_restar(num1, num2)
+	fmt.Println("Los resultados son: ", resultadoSuma, resultadoResta)
+
+	mostrarnum(5, 10, 15, 20, 25)
+	fmt.Println("La sumatoria es: ", sumatoria(1, 2, 3, 4, 5, 6, 7, 8, 9))
+
+}
