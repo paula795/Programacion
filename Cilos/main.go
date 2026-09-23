6aad8219ae7ebd4e58914d3a4f1c0ce0440c9a96
paula795 <pasimbanape@uide.edu.ec>
2026-09-23 18:01:22 -0500

    Repasoprueba

--- Cilos/main.go
+++ Cilos/main.go
@@ -19,3 +19,24 @@ func main() {
 		fmt.Println(rango)
 	}
 }
+
+func Max(a, b int) int {
+
+	if a > b {
+
+		return a
+
+	}
+
+	return b
+
+}
+
+func main() {
+
+	m := Max(5, 8)
+
+	fmt.Println(m)
+	S
+
+}
