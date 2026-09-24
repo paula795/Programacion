8999d5502d9889a67956ef0ce3500de5a38ec366
paula795 <pasimbanape@uide.edu.ec>
2026-09-24 11:00:39 -0500

    Practica1.2

---
+++ Paquetes/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Bienvenidos a la clase de Paquetes ")
+}
---
+++ Paquetes/practica1/operaciones/operaciones.go
@@ -0,0 +1,25 @@
+package operaciones
+
+func suma(a, b int) int {
+	return a + b
+}
+
+func sumaresta(num1, num2 int) (int, int) {
+	if num1 > num2 {
+		ResSuma := num1 + num2
+		ResResta := num1 - num2
+		return ResSuma, ResResta
+	} else {
+		ResSuma := num1 + num2
+		ResResta := 0
+		return ResSuma, ResResta
+	}
+}
+
+func sumatoria(numeros ...int) int {
+	total := 0
+	for _, numero := range numeros {
+		total += numero
+	}
+	return total
+}
